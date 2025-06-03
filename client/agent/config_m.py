caf4c8c5577f6e707d1184bff20bd8cd0667fc86
Mazhorov21Ilia <ilia.liash@yandex.ru>
2025-06-03 11:03:52 +0300

    new

--- client/agent/config_m.py
+++ client/agent/config_m.py
@@ -4,7 +4,7 @@ import yaml
 
 def load_config(config_path="config/config.yaml"):
     if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Не найден файл {config_path}")
+        raise FileNotFoundError(f"Not found {config_path}")
 
     with open(config_path, "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
